dc85a3bacde2cc6f8765d6007ba58b92998fc57b
Ayah Elsais <elsais.ayah@gmail.com>
2023-04-12 00:30:20 -0400

    splash page and route for home set

--- server/seed.py
+++ server/seed.py
@@ -17,18 +17,20 @@ with app.app_context():
     #types include: Nature, Duke Farms, Egypt, Portrait, Event, Grad, Everyday
 
     print("Creating Photos...")
-    
-    nature1 = Photo(description = "Lily of the Valley", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/0SeSPpO")
-    nature2 = Photo(description = "Tall Tree", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/MGZMNPF")
-    nature3 = Photo(description = "purple flower", location = "Deep Cut Gardens Middletown, NJ", film = "", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/kK2Ki8x")
-    nature4 = Photo(description = "Landscape shot", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/qOTLV3G")
-    nature5 = Photo(description = "Bench", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/cOSoBz8")
 
-    dukefarm1 = Photo(description = "Bike Riding", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/Dl5tRrK")
-    dukefarm2 = Photo(description = "In the shade", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/pR9lfSs")
-    dukefarm3 = Photo(description = "call me by your name", location = "Duke Farms", film  = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/c2OznBk")
-    dukefarm4 = Photo(description = "jasmine portrait", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/hj1QUSA")
-    dukefarm5 = Photo(description = "jasmine again", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/fsyCCV1")
+    # add .jpeg to end of imgur link works great!
+    
+    nature1 = Photo(description = "Lily of the Valley", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/0SeSPpO.jpeg")
+    nature2 = Photo(description = "Tall Tree", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/MGZMNPF.jpeg")
+    nature3 = Photo(description = "purple flower", location = "Deep Cut Gardens Middletown, NJ", film = "", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/kK2Ki8x.jpeg")
+    nature4 = Photo(description = "Landscape shot", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/qOTLV3G.jpeg")
+    nature5 = Photo(description = "Bench", location = "Deep Cut Gardens Middletown, NJ", film = "Kodachrome 64", cinebloom_10 = False, type = "Nature", image = "https://imgur.com/cOSoBz8.jpeg")
+
+    dukefarm1 = Photo(description = "Bike Riding", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/Dl5tRrK.jpeg")
+    dukefarm2 = Photo(description = "In the shade", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/pR9lfSs.jpeg")
+    dukefarm3 = Photo(description = "call me by your name", location = "Duke Farms", film  = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/c2OznBk.jpeg")
+    dukefarm4 = Photo(description = "jasmine portrait", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/hj1QUSA.jpeg") 
+    dukefarm5 = Photo(description = "jasmine again", location = "Duke Farms", film = "Kodachrome64", cinebloom_10 = False, type = "Duke Farms", image = "https://imgur.com/fsyCCV1.jpeg")
 
     egypt1 = Photo(description = "", location = "", film = "", cinebloom_10 = False, type = "Egypt", image = "")
 
@@ -102,13 +104,11 @@ with app.app_context():
 
     film_simulations = [kodachrome64, superia100, point_and_shoot]
 
-    camera = Camera(
-        name = "Fujifilm x100v"
-    )
+
 
     db.session.add_all(photos)
     db.session.add_all(film_simulations)
-    db.session.add_all(camera)
+
     db.session.commit()
 
     print("Seeding done!...")
